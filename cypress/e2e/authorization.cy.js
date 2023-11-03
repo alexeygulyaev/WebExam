b972c189fc68b036308686e36978b7a9236a7c1f
Alexey Gulyaev <alexeygulyaev7@gmail.com>
2023-11-04 01:16:35 +0700

    добавил селектор на страницу лец. соглашения

--- cypress/e2e/authorization.cy.js
+++ cypress/e2e/authorization.cy.js
@@ -1,4 +1,5 @@
 it('Authorization page', () => {
   cy.visit('https://80.69.180.108:8441/login');
+  cy.get('app-cookie-notification .mat-card .clickable-text').click()
   cy.get('.mat-card button[data-cy=acceptCookieButton]').click()
       });
